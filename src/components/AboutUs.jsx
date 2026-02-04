b62c5b282003b18963b8b9bae5211bd8c1b02a8e
V R MEHA VENKAT <mehavenkat.outlook@gmail.com>
2026-02-04 20:55:55 +0530

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,1 +1,15 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is an online plant shop that offers a wide variety of
+        indoor plants. Our mission is to make homes greener by providing
+        high-quality plants at affordable prices.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
 

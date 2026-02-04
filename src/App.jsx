ed988a32672a930a6ef6d0bd66d3cf37a6eb9859
V R MEHA VENKAT <mehavenkat.outlook@gmail.com>
2026-02-04 21:03:59 +0530

    Add Get Started button logic to display product list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import "./App.css";
+import ProductList from "./components/ProductList";
 
 const App = () => {
+  const [showProducts, setShowProducts] = useState(false);
+
   return (
     <div className="landing">
-      <div>
-        <h1>Paradise Nursery</h1>
-        <button>Get Started</button>
-      </div>
+      {!showProducts ? (
+        <div>
+          <h1>Paradise Nursery</h1>
+          <button onClick={() => setShowProducts(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 };
 
 export default App;
-

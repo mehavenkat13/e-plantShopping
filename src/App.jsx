13ae4f42c0fbf78003b4e6bd6809c9dce60a9e60
V R MEHA VENKAT <mehavenkat.outlook@gmail.com>
2026-02-04 21:11:32 +0530

    Refactor App component structure and import CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
-import "./App.css";
 import ProductList from "./components/ProductList";
+import "./App.css";
 
-const App = () => {
+function App() {
   const [showProducts, setShowProducts] = useState(false);
 
   return (
-    <div className="landing">
+    <div className="app-container">
       {!showProducts ? (
-        <div>
+        <>
           <h1>Paradise Nursery</h1>
           <button onClick={() => setShowProducts(true)}>
             Get Started
           </button>
-        </div>
+        </>
       ) : (
         <ProductList />
       )}
     </div>
   );
-};
+}
 
 export default App;

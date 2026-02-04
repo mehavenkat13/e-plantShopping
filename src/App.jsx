6dae0842f53de1e44ecf070e49cad0ea7441b270
V R MEHA VENKAT <mehavenkat.outlook@gmail.com>
2026-02-04 20:57:38 +0530

    Add landing page with Get Started button

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="landing">
+      <div>
+        <h1>Paradise Nursery</h1>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
 
